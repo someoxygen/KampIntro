e7dd2d6f6b32a9ca859cad05c6b6bb1b07c48de6
Mustafa Yucel <yucelmustafa37@gmail.com>
2021-05-20 18:42:56 +0300

    20 mayıs ders yukleme 2

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Engin", "Murat", "Mustafa", "Halil" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //isimler = new string[5];
+            //isimler[4] = "Kerem";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimListesi = new List<string> { "Engin", "Murat", "Mustafa", "Halil" };
+            Console.WriteLine(isimListesi[0]);
+            Console.WriteLine(isimListesi[1]);
+            Console.WriteLine(isimListesi[2]);
+            Console.WriteLine(isimListesi[3]);
+            isimListesi.Add("Ilker");
+            Console.WriteLine(isimListesi[4]);
+            Console.WriteLine(isimListesi[0]);
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager  //veya ProductService
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Urun:");
+            Console.WriteLine("Urun Id: ",product.Id);
+            Console.WriteLine("Kategori Id: ",product.CategoryId);
+            Console.WriteLine("Birim Fiyati: ",product.UnitPrice);
+            Console.WriteLine("Stok Adedi: ",product.UnitsInStock);
+
+            Console.WriteLine(product.ProductName + "eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "guncellendi.");
+        }
+
+        
+    }
+}
